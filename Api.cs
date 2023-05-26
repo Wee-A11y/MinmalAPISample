7b156a917673755380ca0d09ce892d50bde3dea0
Allison McHugh <Allison.McHugh@hgem.com>
2023-05-26 15:32:40 +0100

    api

---
+++ Api.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace APIProject;
+
+public static class Api
+{
+    public static void ConfigureApi(this WebApplication app)
+    {
+        app.MapGet("/Users", GetUsers);
+        app.MapGet("/Users/{id}", GetUser);
+        app.MapPost("Users", InsertUser);
+        app.MapPut("/Users", UpdateUser);
+        app.MapDelete("Users", DeleteUser);
+    }
+
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetUsers());
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> GetUser(int id, IUserData data)
+    {
+        try
+        {
+            var results = await data.GetUser(id);
+            return results == null ? Results.NotFound() : Results.Ok(results);
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.InsertUser(user);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+    {
+        try
+        {
+            await data.UpdateUser(user);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteUser(int id, IUserData data)
+    {
+        try
+        {
+            await data.DeleteUser(id);
+            return Results.Ok();
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+    }
+
+}
